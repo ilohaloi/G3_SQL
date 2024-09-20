5a7f96b491423763475728a90e223c6c4aeccf54
BBE <arendtps@gmail.com>
2024-09-20 19:37:25 +0800

    convSQL

---
+++ sql_migrations/V2__Membership.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS  coupon_types;
+DROP TABLE IF EXISTS  member_data;
+DROP TABLE IF EXISTS  user_coupon;
+
+CREATE TABLE coupon_types (
+    coup_id INT PRIMARY KEY auto_increment,
+    coup_name VARCHAR(50) NOT null,
+    coup_dis INT NOT null
+);
+
+CREATE TABLE member_data (
+    memb_id INT PRIMARY KEY auto_increment,
+    memb_name VARCHAR(10) NOT null,
+    memb_email VARCHAR(50) NOT null unique key,
+    memb_tell VARCHAR(10),
+    memb_address VARCHAR(80),
+    memb_birthday date,
+ 	memb_password varchar(24) NOT null
+ );
+
+CREATE TABLE user_coupon (
+    coup_no INT PRIMARY KEY auto_increment,
+    memb_id INT,
+    coup_id INT,
+    coup_status varchar(20),
+    CONSTRAINT fk_user_coupon_member_data_memb_id
+    FOREIGN KEY (memb_id) REFERENCES member_data(memb_id),
+    CONSTRAINT fk_user_coupon_coupon_types_coup_id
+    FOREIGN KEY (coup_id) REFERENCES coupon_types(coup_id)
+);
