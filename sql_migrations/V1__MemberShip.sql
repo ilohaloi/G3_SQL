b24c7344b084280b618ebe601463dde8e493f300
applechi0130 <doris19751@gmail.com>
2024-10-21 15:19:32 +0800

    更新優惠券

--- sql_migrations/V1__MemberShip.sql
+++ sql_migrations/V1__MemberShip.sql
@@ -4,12 +4,9 @@ DROP TABLE IF EXISTS  coupon_types;
 
 CREATE TABLE coupon_type (
     coup_id INT PRIMARY KEY auto_increment,
-    coup_name VARCHAR(50) NOT null,
+    coup_code VARCHAR(50) NOT null,
     coup_discount Double NOT null,
-    coup_description varchar(255),
-    coup_issue_date Timestamp,
-    coup_expiry_date Timestamp,
-	coup_is_used TINYINT(1) DEFAULT 0
+    coup_description varchar(255)
 );
 
 CREATE TABLE member_data (
@@ -24,11 +21,14 @@ CREATE TABLE member_data (
 
 CREATE TABLE user_coupon (
     coup_no INT PRIMARY KEY auto_increment,
-    memb_id INT,
-    coup_id INT,
-    coup_status varchar(20),
+    memb_id INT,  
+    coup_id INT, 
+    coup_issue_date TIMESTAMP, 
+    coup_expiry_date TIMESTAMP, 
+    coup_is_used TINYINT(1) DEFAULT 0,  
     CONSTRAINT fk_user_coupon_member_data_memb_id
-    FOREIGN KEY (memb_id) REFERENCES member_data(memb_id),
+        FOREIGN KEY (memb_id) REFERENCES member_data(memb_id), 
     CONSTRAINT fk_user_coupon_coupon_types_coup_id
-    FOREIGN KEY (coup_id) REFERENCES coupon_type(coup_id)
+        FOREIGN KEY (coup_id) REFERENCES coupon_type(coup_id) 
 );
+
--- sql_migrations/V5__coup.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE coupon_type
-ADD CONSTRAINT fk_member_id
-FOREIGN KEY (memb_id) REFERENCES member_data(memb_id);
